6d92d8a96e7369477184ad50e7b7962c2642f4e3
Uzi Landsmann <uzi.landsmann@bisnode.com>
2021-12-05 08:24:12 +0100

    Updated day 5

--- src/test/kotlin/adventofcode/Day5Test.kt
+++ src/test/kotlin/adventofcode/Day5Test.kt
@@ -7,10 +7,10 @@ class Day5Test : StringSpec({
     val input = Utils.readLines("day5test.txt")
 
     "Day 5 part 1" {
-        Day5.part1(input,9) shouldBe 5
+        Day5.part1(input, 9) shouldBe 5
     }
 
     "Day 5 part 2" {
-        Day5.part2(input,9) shouldBe 12
+        Day5.part2(input, 9) shouldBe 12
     }
 })
